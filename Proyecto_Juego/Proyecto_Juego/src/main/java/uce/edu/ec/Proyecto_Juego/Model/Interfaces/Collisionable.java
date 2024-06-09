5a6263d7cfa474a190b59da627f561af5f79694d
Dario Vergara <dcvergara@uce.edu.ec>
2024-06-09 18:21:51 -0500

    Version Beta

--- Proyecto_Juego/Proyecto_Juego/src/main/java/uce/edu/ec/Proyecto_Juego/Model/Interfaces/Collisionable.java
+++
@@ -1,9 +0,0 @@
-package uce.edu.ec.Proyecto_Juego.Model.Interfaces;
-
-import uce.edu.ec.Proyecto_Juego.Model.Figure.Bullet;
-
-public interface Collisionable {
-
-   boolean Collision(Bullet bullet);
-
-}
--- basicJPA/src/main/java/ec/uce/edu/basicJPA/BasicJpaApplication.java
+++ basicJPA/src/main/java/ec/uce/edu/basicJPA/BasicJpaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicJpaApplication  {
+public class 	BasicJpaApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicJpaApplication.class, args);
