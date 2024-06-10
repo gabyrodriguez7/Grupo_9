7858cdb9cd163c15adb2faed000704e08adabf21
Dario Vergara <dcvergara@uce.edu.ec>
2024-06-09 21:44:18 -0500

    Actualizacion base de datos 1.1

--- basicJPA/src/main/java/ec/uce/edu/basicJPA/BasicJpaApplication.java
+++ basicJPA/src/main/java/ec/uce/edu/basicJPA/BasicJpaApplication.java
@@ -3,6 +3,14 @@ package ec.uce.edu.basicJPA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author: John Steven Quinatoa, Gabriela Lissette Rodriguez Contreras, Dario Calixto Vergara Ortiz
+ *
+ *
+ *
+ * Tema: Juego Galaga
+ */
+
 @SpringBootApplication
 public class 	BasicJpaApplication  {
 
